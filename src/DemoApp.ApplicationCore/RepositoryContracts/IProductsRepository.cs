24be201c718b5150ef337b2d5c717dc5b16e6a3b
salkam3156 <salkam3156@hotmail.com>
2021-08-16 21:55:06 +0200

    added products repository contract ,added infrastructure project

---
+++ src/DemoApp.ApplicationCore/RepositoryContracts/IProductsRepository.cs
@@ -0,0 +1,13 @@
+﻿using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.Enumerations;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+
+namespace DemoApp.ApplicationCore.RepositoryContracts
+{
+    public interface IProductsRepository
+    {
+        public Task<Option<IEnumerable<Product>, RepositoryFailure>> FindProductsBelowPrice(float price);
+    }
+}
