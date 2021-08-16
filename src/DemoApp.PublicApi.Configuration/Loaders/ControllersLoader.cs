a16c21819c6f0e701f383e44711bf0c583f14f86
salkam3156 <salkam3156@hotmail.com>
2021-08-16 21:08:56 +0200

    wrote an assembly laoder for controllers plugin assemblies

---
+++ src/DemoApp.PublicApi.Configuration/Loaders/ControllersLoader.cs
@@ -0,0 +1,32 @@
+﻿using DemoApp.PublicApi.Configuration.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace DemoApp.PublicApi.Configuration.Loaders
+{
+    internal static class ControllersLoader
+    {
+        public static IServiceCollection AddControllersFromExternalAssembly(this IServiceCollection serviceCollection, Plugins options)
+        {
+             if (options is null) throw new ArgumentException(
+                    $"Unable to load API controllers. Please provide configuration values in {nameof(Plugins)}");
+
+            var controllersAssembly = GetConfiguredControllersAssembly(options);
+
+            serviceCollection
+                .AddControllers()
+                .AddApplicationPart(controllersAssembly)
+                .AddControllersAsServices();
+
+            return serviceCollection;
+        }
+
+        private static Assembly GetConfiguredControllersAssembly(Plugins options)
+        {
+            var controllersAssemblyName = options.ControllersAssembly;
+
+            return Assembly.Load(controllersAssemblyName);
+        }
+    }
+}
