00f3c554a71f6c2dcffbf7155ca0d73a26ca2837
salkam3156 <salkam3156@hotmail.com>
2021-08-16 22:28:21 +0200

    formatting in option class

--- src/DemoApp.ApplicationCore/GeneralAbstractions/Option.cs
+++ src/DemoApp.ApplicationCore/GeneralAbstractions/Option.cs
@@ -11,8 +11,11 @@ namespace DemoApp.ApplicationCore.GeneralAbstractions
         private readonly TValidResult _result = default;
         private readonly TFailure _status = default;
         private readonly bool _isValidResult = false;
-        public Option(TValidResult result) => (_result, _isValidResult) = (result, true);
-        public Option(TFailure error) => (_status, _isValidResult) = (error, false);
+        public Option(TValidResult result) 
+            => (_result, _isValidResult) = (result, true);
+
+        public Option(TFailure error) 
+            => (_status, _isValidResult) = (error, false);
 
         public T Extract<T>(Func<TValidResult, T> validResult, Func<TFailure, T> errored) 
             => _isValidResult ? validResult(this._result) : errored(this._status);
