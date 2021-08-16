10fa381fed75b706ff18144e06924c7c8b5496e7
salkam3156 <salkam3156@hotmail.com>
2021-08-17 00:37:33 +0200

    added tests for app core

---
+++ src/DemoApp.ApplicationCore.Tests/OptionTests.cs
@@ -0,0 +1,59 @@
+using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using FluentAssertions;
+using Xunit;
+
+namespace DemoApp.ApplicationCore.Tests
+{
+
+    namespace DemoApp.ApplicationCore.Tests.GenericAbstractions
+    {
+        public class OptionTests
+        {
+            private class SomeFailureObject
+            {
+                public string FailureMessage => "Operation failed";
+            }
+
+            [Fact]
+            public void ShouldProvideResultOnSuccessfulOperation()
+            {
+                // given
+                var validResult = new Product(1, "TestProduct", 1.25f, "This is a test product");
+
+                // when
+                var resultOption = new Option<Product, SomeFailureObject>(validResult);
+
+                var result = resultOption.Extract(
+                    validResult => validResult,
+                    failure => default
+                    );
+
+                // then
+                result.Should().BeOfType(typeof(Product)).And.BeSameAs(validResult);
+            }
+
+            [Fact]
+            public void ShouldMakeDetailsAccessibleOnFailure()
+            {
+                // given
+                var failure = new SomeFailureObject();
+                var failureMessage = string.Empty;
+
+                // when
+                var resultOption = new Option<Product, SomeFailureObject>(failure);
+
+                var result = resultOption.Extract(
+                    validResult => validResult,
+                    failure => { failureMessage = failure.FailureMessage; return default; }
+                    );
+
+                // then
+                failureMessage.Should().BeSameAs(failure.FailureMessage);
+
+                result.Should().BeSameAs(default);
+            }
+        }
+    }
+
+}
