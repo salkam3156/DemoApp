8717e48a9e5a0493c3f8c48f7787a29ef9a37e3f
salkam3156 <salkam3156@hotmail.com>
2021-08-16 22:29:02 +0200

    added EFCore and generic find method implementation

--- src/DemoApp.Infrastructure/Repositories/AsyncRepositoryBase.cs
+++ src/DemoApp.Infrastructure/Repositories/AsyncRepositoryBase.cs
@@ -1,8 +1,10 @@
 ﻿using DemoApp.ApplicationCore.Enumerations;
 using DemoApp.ApplicationCore.GeneralAbstractions;
 using DemoApp.ApplicationCore.RepositoryContracts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
@@ -10,14 +12,35 @@ namespace DemoApp.Infrastructure.Repositories
 {
     public class AsyncRepositoryBase<TEntity> : IAsyncReposiroty<TEntity> where TEntity : class
     {
+        protected readonly DbContext Ctx;
+        public AsyncRepositoryBase(DbContext ctx) 
+            => Ctx = ctx;
+
         public Task<RepositoryFailure> AddAsync(TEntity collection)
         {
             throw new System.NotImplementedException();
         }
         
-        public Task<Option<TEntity, RepositoryFailure>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+        public async Task<Option<TEntity, RepositoryFailure>> FindAsync(Expression<Func<TEntity, bool>> predicate)
         {
-            throw new System.NotImplementedException();
+            TEntity queryResult = default;
+            try
+            {
+                queryResult = await Ctx.Set<TEntity>().Where(predicate).SingleOrDefaultAsync();
+            }
+            catch (Exception ex) when (ex is InvalidOperationException io || ex is  ArgumentNullException an)
+            {
+                //TODO: add logging
+                return new Option<TEntity, RepositoryFailure>(RepositoryFailure.GeneralExecutionFailure);
+            }
+
+            return queryResult switch
+            {
+                _ when queryResult is TEntity => new Option<TEntity, RepositoryFailure>(queryResult),
+                null => new Option<TEntity, RepositoryFailure>(RepositoryFailure.NotFound),
+
+                _ => new Option<TEntity, RepositoryFailure>(RepositoryFailure.UnspecifiedFailure)
+            };
         }
 
         public Task<Option<IEnumerable<TEntity>, RepositoryFailure>> GetAllAsync()
