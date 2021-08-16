80c06375474ed6458af0305992ddd452d6beb787
salkam3156 <salkam3156@hotmail.com>
2021-08-16 22:01:42 +0200

    added a stub for a generic repository base implementation

---
+++ src/DemoApp.Infrastructure/Repositories/AsyncRepositoryBase.cs
@@ -0,0 +1,36 @@
+﻿using DemoApp.ApplicationCore.Enumerations;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using DemoApp.ApplicationCore.RepositoryContracts;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DemoApp.Infrastructure.Repositories
+{
+    public class AsyncRepositoryBase<TEntity> : IAsyncReposiroty<TEntity> where TEntity : class
+    {
+        public Task<RepositoryFailure> AddAsync(TEntity collection)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        public Task<Option<TEntity, RepositoryFailure>> FindAsync(System.Linq.Expressions.Expression<System.Func<TEntity, bool>> predicate)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Option<IEnumerable<TEntity>, RepositoryFailure>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Option<TEntity, RepositoryFailure>> GetAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<RepositoryFailure> RemoveAsync(TEntity collection)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
