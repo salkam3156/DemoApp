450bf3733809bc0899a82cc13417ba616eb05f53
salkam3156 <salkam3156@hotmail.com>
2021-08-16 21:39:17 +0200

    addded generic repository infrastructure deman in core. Added failure abstraction for error reporting

---
+++ src/DemoApp.ApplicationCore/Enumerations/RepositoryFailure.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoApp.ApplicationCore.Enumerations
+{
+    public enum RepositoryFailure
+    {
+        NotFound = 1,
+        UnableToCreate = 2,
+        
+        GeneralExecutionFailure = 3,
+        UnspecifiedFailure = 4,
+
+    }
+}
---
+++ src/DemoApp.ApplicationCore/RepositoryContracts/IAsyncRepository.cs
@@ -0,0 +1,21 @@
+﻿using DemoApp.ApplicationCore.Enumerations;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace DemoApp.ApplicationCore.RepositoryContracts
+{
+    public class IAsyncRepository
+    {
+        public interface IRepository<TEntity> where TEntity : class
+        {
+            Task<RepositoryFailure> AddAsync(TEntity collection);
+            Task<RepositoryFailure> RemoveAsync(TEntity collection);
+            Task<Option<TEntity, RepositoryFailure>> GetAsync(int id);
+            Task<Option<IEnumerable<TEntity>, RepositoryFailure>> GetAllAsync();
+            Task<Option<TEntity, RepositoryFailure>> FindAsync(Expression<Func<TEntity, bool>> predicate);
+        }
+    }
+}
