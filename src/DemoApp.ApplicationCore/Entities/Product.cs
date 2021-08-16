d150dce3602e1bb13ec71fe2d79282d4396fc7c9
salkam3156 <salkam3156@hotmail.com>
2021-08-16 21:39:57 +0200

    added aneminc domain model stub for testing. Preparing to introduce infrastructure implementations

---
+++ src/DemoApp.ApplicationCore/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DemoApp.ApplicationCore.Entities
+{
+    public sealed record Product
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public float Price { get; init; }
+        public string Description { get; init; }
+
+    }
+}
