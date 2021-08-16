c46e11356c6182805a4adf1859286eaff36d924a
salkam3156 <salkam3156@hotmail.com>
2021-08-17 01:10:05 +0200

    typed database access exception

---
+++ src/DemoApp.Infrastructure/Exceptions/Database/DatabaseAccessException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoApp.Infrastructure.Exceptions.Database
+{
+    [Serializable]
+    public class DatabaseAccessException : Exception
+    {
+        public DatabaseAccessException(string? message) : base(message)
+        {
+        }
+    }
+}
