ada975cffae20e936de1269324eb2e986281a53d
salkam3156 <salkam3156@hotmail.com>
2021-08-17 01:11:33 +0200

    added products repository and products db ctx in preparation for in memory db initializer for ease of development

---
+++ src/DemoApp.Infrastructure/Repositories/DbContexts/ProductsDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DemoApp.ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoApp.Infrastructure.Repositories.DbContexts
+{
+    public sealed class ProductsDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        
+        public ProductsDbContext(DbContextOptions<ProductsDbContext> contextOptions) : base(contextOptions) { }
+    }
+}
---
+++ src/DemoApp.Infrastructure/Repositories/Implementations/ProductsRepository.cs
@@ -0,0 +1,32 @@
+﻿
+using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.Enumerations;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using DemoApp.ApplicationCore.RepositoryContracts;
+using DemoApp.Infrastructure.Exceptions.Database;
+using DemoApp.Infrastructure.Repositories.DbContexts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp.Infrastructure.Repositories.Implementations
+{
+    public sealed class ProductsRepository : AsyncRepositoryBase<Product>, IProductsRepository
+    {
+        public ProductsRepository(ProductsDbContext ctx) : base(ctx) {}
+        public ProductsDbContext ProductsContext
+            => Ctx as ProductsDbContext 
+            ?? throw new DatabaseAccessException($"{nameof(ProductsContext)} could not be accessed due to failed DB dependency.");
+
+        public Task<Option<IEnumerable<Product>, RepositoryFailure>> FindProductsBelowPrice(float price)
+        {
+            var productsBelowPrice = ProductsContext
+                .Products
+                .Where(product => product.Price < price);
+            
+            return Task.FromResult(
+                new Option<IEnumerable<Product>, RepositoryFailure>(productsBelowPrice)
+                );
+        }
+    }
+}
