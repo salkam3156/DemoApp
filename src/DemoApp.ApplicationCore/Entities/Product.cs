d9194f0a78aaa834967f39ff65cb43c0ba219f43
salkam3156 <salkam3156@hotmail.com>
2021-08-16 22:07:20 +0200

    simplifying product for now and added a TODO for ctor and validation

--- src/DemoApp.ApplicationCore/Entities/Product.cs
+++ src/DemoApp.ApplicationCore/Entities/Product.cs
@@ -1,12 +1,6 @@
 ﻿
 namespace DemoApp.ApplicationCore.Entities
 {
-    public sealed record Product
-    {
-        public int Id { get; init; }
-        public string Name { get; init; }
-        public float Price { get; init; }
-        public string Description { get; init; }
-
-    }
+    //TODO: flesh out, introduce validation in ctor
+    public sealed record Product(int Id, string Name, float Price, string Description);
 }
