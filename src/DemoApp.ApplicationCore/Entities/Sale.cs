d4c73e9fbb1bd86dc6faf65a005561d733803201
salkam3156 <salkam3156@hotmail.com>
2021-08-18 01:36:50 +0200

    extended sale with datetime

--- src/DemoApp.ApplicationCore/Entities/Sale.cs
+++ src/DemoApp.ApplicationCore/Entities/Sale.cs
@@ -1,4 +1,5 @@
 ﻿using DemoApp.ApplicationCore.GeneralAbstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -9,7 +10,9 @@ namespace DemoApp.ApplicationCore.Entities
         public int Id { get; init; }
         public decimal ApplicableTax { get; init; }
         public IEnumerable<Product> ProductsSold { get; init; }
-        public Sale(int id, decimal applicableTax, IEnumerable<Product> productsSold)
+        public DateTime SoldOn { get; init; }
+
+        public Sale(int id, decimal applicableTax, DateTime soldOn, IEnumerable<Product> productsSold)
         {
             Validation.ThrowIfAnyAreFalse(
                 () => id >= 1,
@@ -19,6 +22,7 @@ namespace DemoApp.ApplicationCore.Entities
             Id = id;
             ApplicableTax = applicableTax;
             ProductsSold = productsSold;
+            SoldOn = DateTime.Now;
         }
 
         private Sale() { }
