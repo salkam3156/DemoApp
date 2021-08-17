7c0f9b8611b1bdd4081dd14755f06011c0c63c22
salkam3156 <salkam3156@hotmail.com>
2021-08-18 01:21:53 +0200

    validation logic tests

---
+++ src/DemoApp.ApplicationCore.Tests/ValidationTests.cs
@@ -0,0 +1,37 @@
+﻿using DemoApp.ApplicationCore.Exceptions;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace DemoApp.ApplicationCore.Tests.GenericAbstractions
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void ShouldThrowIfAnyAreFalse()
+        {
+            Action validationAction = ()
+                => Validation.ThrowIfAnyAreFalse(
+                    () => 1 == 1,
+                    () => 1 == 0);
+
+            validationAction
+                .Should()
+                .ThrowExactly<ObjectCreationException>()
+                .WithMessage("Validation failed while creating a domain object.");
+        }
+
+        [Fact]
+        public void ShouldNotThrowIfAllTrue()
+        {
+            Action validationAction = ()
+                => Validation.ThrowIfAnyAreFalse(
+                    () => true is true,
+                    () => true,
+                    () => 1 == 1);
+
+            validationAction.Should().NotThrow();
+        }
+    }
+}
