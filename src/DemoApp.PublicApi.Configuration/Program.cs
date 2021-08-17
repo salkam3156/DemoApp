98cc9c5b0aa6ce1d8921fd2ee53d1bc878a54dac
salkam3156 <salkam3156@hotmail.com>
2021-08-18 01:52:56 +0200

    hacked db initialization in to host startup in debug

--- src/DemoApp.PublicApi.Configuration/Program.cs
+++ src/DemoApp.PublicApi.Configuration/Program.cs
@@ -1,13 +1,28 @@
+using DemoApp.Infrastructure.Repositories.DbContexts;
+using DemoApp.PublicApi.Configuration.Development;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
 
 namespace DemoApp.PublicApi.Configuration
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hostBuilder = CreateHostBuilder(args).Build();
+
+#if DEBUG
+            using var dbContext = hostBuilder.Services
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<DomainContext>();
+
+            await InMemoryDbInitializer.Initialize(dbContext);
+#endif
+
+            await hostBuilder.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
