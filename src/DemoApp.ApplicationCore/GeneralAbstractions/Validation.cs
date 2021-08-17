95e983fc87426bcedd8a8c24cbe7448f152f5914
salkam3156 <salkam3156@hotmail.com>
2021-08-18 00:46:48 +0200

    added general purpose validation method

---
+++ src/DemoApp.ApplicationCore/GeneralAbstractions/Validation.cs
@@ -0,0 +1,21 @@
+﻿using DemoApp.ApplicationCore.Exceptions;
+using System;
+
+namespace DemoApp.ApplicationCore.GeneralAbstractions
+{
+    public static class Validation
+    {
+        public static void ThrowIfAnyAreFalse(params Func<bool>[] validationPredicates)
+        {
+            foreach(var predicate in validationPredicates)
+            {
+                if (predicate is null) // being defensive here / futureproof
+                    throw new ObjectCreationException("Invalid validation rule provided. Did you intend that ? The object cannot be created.");
+
+                if(predicate.Invoke() is false) 
+                    throw new ObjectCreationException("Validation failed while creating a domain object.");
+            };
+            
+        }
+    }
+}
