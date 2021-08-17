d0bf4a0ccc86b25b63cc60a2be8074a7c657aca5
salkam3156 <salkam3156@hotmail.com>
2021-08-18 01:02:39 +0200

    bogus sale model to have more things to work with + validation

---
+++ src/DemoApp.ApplicationCore/Entities/Sale.cs
@@ -0,0 +1,28 @@
+﻿using DemoApp.ApplicationCore.GeneralAbstractions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoApp.ApplicationCore.Entities
+{
+    public sealed record Sale
+    {
+        public int Id { get; init; }
+        public decimal ApplicableTax { get; init; }
+        public IEnumerable<Product> ProductsSold { get; init; }
+        public Sale(int id, decimal applicableTax, IEnumerable<Product> productsSold)
+        {
+            Validation.ThrowIfAnyAreFalse(
+                () => id >= 1,
+                () => applicableTax > 0,
+                () => productsSold is not null && productsSold.Any());
+
+            Id = id;
+
+            ApplicableTax = applicableTax;
+
+            ProductsSold = productsSold;
+        }
+
+        private Sale() { }
+     }
+}
