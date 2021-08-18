cf57ccca4cc4b6d88324f239825c045c13db1556
salkam3156 <salkam3156@hotmail.com>
2021-08-18 02:20:09 +0200

    added product dto

---
+++ src/DemoApp.PublicApi.Controllers/DTO/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿using DemoApp.ApplicationCore.Entities;
+
+namespace DemoApp.PublicApi.Controllers.DTO
+{
+    public class ProductDto
+    {
+        public string Name { get; init; }
+        public ProductType Type { get; init; }
+        public decimal Price { get; init; }
+        public string Producer { get; init; }
+        public string Description { get; init; }
+    }
+}
