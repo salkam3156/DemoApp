4c92c4c0cb66057879f9a3a3034b5d8ba0ccd0c0
salkam3156 <salkam3156@hotmail.com>
2021-08-18 02:00:08 +0200

    glued things together for a usable feature, added automapper. Moving on to in-process messaging to remove repo reference

--- src/DemoApp.PublicApi.Controllers/ProductsController.cs
+++ src/DemoApp.PublicApi.Controllers/ProductsController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.RepositoryContracts;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DemoApp.PublicApi.Controllers
 {
@@ -7,12 +12,22 @@ namespace DemoApp.PublicApi.Controllers
     [Route("api/[controller]")]
     public sealed class ProductsController : ControllerBase
     {
+        private readonly IProductsRepository _productsRepository;
+        public ProductsController(IProductsRepository productsRepository)
+            => _productsRepository = productsRepository;
+
         [HttpGet]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Product>))]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult GetAllProducts(/*param*/)
+        public async Task<IActionResult> GetProductsBelowPrice([FromQuery] decimal price)
         {
-            return Ok("Swagger test");
+            var productsBelowPrice = await _productsRepository.FindProductsBelowPriceAsync(price);
+
+            var extractedResult = productsBelowPrice.Extract(
+                products => products,
+                fetchFailure => Enumerable.Empty<Product>());
+            
+            return Ok(extractedResult);
         }
     }
 }
