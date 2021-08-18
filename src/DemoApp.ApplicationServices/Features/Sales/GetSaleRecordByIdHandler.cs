af0b35a1fbb38a90e8f31a8240cc08df9a895e73
salkam3156 <salkam3156@hotmail.com>
2021-08-19 01:40:55 +0200

    added get sale by id feature

---
+++ src/DemoApp.ApplicationServices/Features/Sales/GetSaleRecordByIdHandler.cs
@@ -0,0 +1,17 @@
+﻿using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.Enums;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoApp.ApplicationServices.Features.Sales
+{
+    public class GetSaleRecordByIdHandler : IRequestHandler<GetSaleRecordByIdRequest, Result<Sale, DataAccessResult>>
+    {
+        public Task<Result<Sale, DataAccessResult>> Handle(GetSaleRecordByIdRequest request, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/DemoApp.ApplicationServices/Features/Sales/GetSaleRecordByIdRequest.cs
@@ -0,0 +1,15 @@
+﻿using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.Enums;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using MediatR;
+
+namespace DemoApp.ApplicationServices.Features.Sales
+{
+    public sealed record GetSaleRecordByIdRequest : IRequest<Result<Sale, DataAccessResult>>
+    {
+        public int SaleId { get; private init; }
+
+        public GetSaleRecordByIdRequest(int saleId)
+            => SaleId = saleId;
+    }
+}
