78505fdbeeb18aac96502bc2ed738860fad96551
salkam3156 <salkam3156@hotmail.com>
2021-08-18 02:49:35 +0200

    added a notification infrastructure demand in the application. Another service-type functionality

---
+++ src/DemoApp.ApplicationCore/MessagingContracts/INotifier.cs
@@ -0,0 +1,10 @@
+﻿using DemoApp.ApplicationCore.Models;
+using System.Threading.Tasks;
+
+namespace DemoApp.ApplicationCore.MessagingContracts
+{
+    public interface INotifier
+    {
+        Task NotifyClientsAsync(Notification notification);
+    }
+}
