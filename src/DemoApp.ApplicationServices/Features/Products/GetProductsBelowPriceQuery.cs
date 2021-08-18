e8239d2562dcc153d513b01971c79ded0f55af2b
salkam3156 <salkam3156@hotmail.com>
2021-08-19 00:18:42 +0200

    added app feature definition to call on / request

---
+++ src/DemoApp.ApplicationServices/Features/Products/GetProductsBelowPriceQuery.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.Enums;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using MediatR;
+
+namespace DemoApp.ApplicationServices.Features.Products
+{
+    public sealed record GetProductsBelowPriceQuery : IRequest<Result<IEnumerable<Product>, DataAccessResult>>
+    {
+        public decimal Price { get; init; }
+
+        public GetProductsBelowPriceQuery(decimal price)
+            => Price = price;
+    }
+}
