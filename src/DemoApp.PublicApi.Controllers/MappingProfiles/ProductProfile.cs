1a9bd29527174df58cfdfafc40f653a20113a2c7
salkam3156 <salkam3156@hotmail.com>
2021-08-18 02:24:26 +0200

    added a product mapping profile

---
+++ src/DemoApp.PublicApi.Controllers/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DemoApp.ApplicationCore.Entities;
+using DemoApp.PublicApi.Controllers.DTO;
+
+namespace DemoApp.PublicApi.Controllers.MappingProfiles
+{
+    internal sealed class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>();
+        }
+    }
+}
