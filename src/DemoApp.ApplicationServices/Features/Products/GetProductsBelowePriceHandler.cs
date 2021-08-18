1bf431b742135f1112c9faa661e72e9366976e1b
salkam3156 <salkam3156@hotmail.com>
2021-08-19 00:21:46 +0200

    added feature request handler

---
+++ src/DemoApp.ApplicationServices/Features/Products/GetProductsBelowePriceHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.Enums;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using DemoApp.ApplicationCore.RepositoryContracts;
+using MediatR;
+
+namespace DemoApp.ApplicationServices.Features.Products
+{
+    public class GetProductsBelowePriceHandler : IRequestHandler<GetProductsBelowPriceQuery, Result<IEnumerable<Product>, DataAccessResult>>
+    {
+        private readonly IProductsRepository _productsRepository;
+
+        public GetProductsBelowePriceHandler(IProductsRepository productsRepository)
+            => _productsRepository = productsRepository;
+        
+        public async Task<Result<IEnumerable<Product>, DataAccessResult>> Handle(GetProductsBelowPriceQuery request, CancellationToken cancellationToken)
+        {
+            return await _productsRepository.FindProductsBelowPriceAsync(request.Price);
+        }
+    }
+}
