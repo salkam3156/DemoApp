4833139ab641755d04b0de8eea5753505a4e8399
salkam3156 <salkam3156@hotmail.com>
2021-08-19 05:24:03 +0200

    get products query - missing commit

---
+++ src/DemoApp.ApplicationServices/Features/Products/GetProductsQuery.cs
@@ -0,0 +1,12 @@
+﻿using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.Enums;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using MediatR;
+using System.Collections.Generic;
+
+namespace DemoApp.ApplicationServices.Features.Products
+{
+    public sealed class GetProductsQuery : IRequest<Result<IEnumerable<Product>, DataAccessResult>>
+    {
+    }
+}
