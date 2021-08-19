61a620f914080da3e038780bacfe30702747cc90
salkam3156 <salkam3156@hotmail.com>
2021-08-19 05:21:06 +0200

    added sales request response

---
+++ src/DemoApp.PublicApi.Controllers/DTO/SaleCreationRequestDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DemoApp.PublicApi.Controllers.DTO
+{
+    public class SaleCreationRequestDto
+    {
+        public List<int> ProductIds{ get; set; }
+    }
+}
---
+++ src/DemoApp.PublicApi.Controllers/DTO/SaleResponseDto.cs
@@ -0,0 +1,14 @@
+﻿
+using DemoApp.ApplicationCore.Entities;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace DemoApp.PublicApi.Controllers.DTO
+{
+    public sealed record SaleResponseDto
+    {
+        public List<Product> ProductsSold { get; init; }
+        public DateTime SoldOn { get; init; }
+    }
+}
