b0243f206f4ea5bb184210d61b04a1e2d1ee38cc
salkam3156 <salkam3156@hotmail.com>
2021-08-20 00:33:05 +0200

    removed DTO

--- src/DemoApp.PublicApi.Controllers/DTO/SaleCreationRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace DemoApp.PublicApi.Controllers.DTO
-{
-    public class SaleCreationRequestDto
-    {
-        public List<int> ProductIds{ get; set; }
-    }
-}
