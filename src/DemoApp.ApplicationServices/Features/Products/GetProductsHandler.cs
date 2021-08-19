4788eb2502bbfa6d00e7eeca0bddcea7afa8065b
salkam3156 <salkam3156@hotmail.com>
2021-08-19 05:23:06 +0200

    quick get-all products handler impl

---
+++ src/DemoApp.ApplicationServices/Features/Products/GetProductsHandler.cs
@@ -0,0 +1,28 @@
+﻿using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.Enums;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using DemoApp.ApplicationCore.RepositoryContracts;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoApp.ApplicationServices.Features.Products
+{
+    public sealed class GetProductsHandler : IRequestHandler<GetProductsQuery, Result<IEnumerable<Product>, DataAccessResult>>
+    {
+        private readonly IProductsRepository _productsRepository;
+
+        public GetProductsHandler(IProductsRepository productsRepository)
+            => _productsRepository = productsRepository;
+        
+        public async Task<Result<IEnumerable<Product>, DataAccessResult>> Handle(GetProductsQuery request, CancellationToken cancellationToken)
+        {
+            var repositoryCrudBase = _productsRepository as IAsyncReposiroty<Product>;
+
+            var allProducts = await repositoryCrudBase.GetAllAsync();
+
+            return allProducts;
+        }
+    }
+}
