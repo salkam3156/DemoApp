1e131a48d8e0e6920e66512037d56666258f02e9
salkam3156 <salkam3156@hotmail.com>
2021-08-19 05:17:44 +0200

    hacky impl to put things together functionally, to be optimized

--- src/DemoApp.Infrastructure/Repositories/Implementations/ProductsRepository.cs
+++ src/DemoApp.Infrastructure/Repositories/Implementations/ProductsRepository.cs
@@ -27,5 +27,20 @@ namespace DemoApp.Infrastructure.Repositories.Implementations
                 new Result<IEnumerable<Product>, DataAccessResult>(productsBelowPrice)
                 );
         }
+
+        public async Task<Result<IEnumerable<Product>, DataAccessResult>> FindProductsByIdsAsync(IEnumerable<int> ids)
+        {
+            // TODO: change in the future to "not dumb" version / thread pool starvation indeterminate number of method calls etc . Should be a match / join
+            var matchedProductsTasks = ids.Select(async id => await AppContext.Products.FindAsync(id));
+            var mathedProducts = (await Task.WhenAll(matchedProductsTasks)).Where(product => product is not null).ToList();
+
+            //TODO: this instantiates 2 enumerators, so think about that
+            return (mathedProducts.Count() == ids.Count()) switch
+            {
+                true => new Result<IEnumerable<Product>, DataAccessResult>(mathedProducts),
+                // TODO: change the DataAccessResult to a more robuste type, template it and provide difference a la metched.Except(expected) or a text report
+                false => new Result<IEnumerable<Product>, DataAccessResult>(DataAccessResult.NotAllFound)
+            };
+        }
     }
 }
