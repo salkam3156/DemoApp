13a4c867f0c2cec26e1240dadea66e1816bc2c67
salkam3156 <salkam3156@hotmail.com>
2021-08-19 05:23:39 +0200

    get products by ids feature

---
+++ src/DemoApp.ApplicationServices/Features/Products/GetProductsByIdsRequest.cs
@@ -0,0 +1,15 @@
+﻿using DemoApp.ApplicationCore.Entities;
+using DemoApp.ApplicationCore.Enums;
+using DemoApp.ApplicationCore.GeneralAbstractions;
+using MediatR;
+using System.Collections.Generic;
+
+namespace DemoApp.ApplicationServices.Features.Products
+{
+    public sealed class GetProductsByIdsRequest : IRequest<Result<IEnumerable<Product>, DataAccessResult>>
+    {
+        public int[] ProductIds { get; private init; }
+        public GetProductsByIdsRequest(int[] productIds)
+            => ProductIds = productIds;
+    }
+}
