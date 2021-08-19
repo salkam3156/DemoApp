678ee93b2c1371623e806bd663cec9eae5f4c126
salkam3156 <salkam3156@hotmail.com>
2021-08-20 01:03:27 +0200

    taxregion model validation in ctor

--- src/DemoApp.ApplicationServices/Models/TaxRegion.cs
+++ src/DemoApp.ApplicationServices/Models/TaxRegion.cs
@@ -1,4 +1,15 @@
-﻿namespace DemoApp.ApplicationServices.Models
+﻿using DemoApp.ApplicationCore.GeneralAbstractions;
+
+namespace DemoApp.ApplicationServices.Models
 {
-    public sealed record TaxRegion(string Region);
+    public sealed record TaxRegion
+    {
+        public string Region { get; private init; }   
+        public TaxRegion(string region) 
+        {
+            Validation.ThrowIfAnyAreFalse(() => string.IsNullOrWhiteSpace(region) is false);
+
+            Region = region;
+        }
+    }
 }
